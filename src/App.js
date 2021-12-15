01353a524b63fbd77944f7e6ec9b7eead02e4936
YoniEtedgi <yonie812@gmail.com>
2021-12-15 15:29:46 +0200

    Updating useState on array

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
+const Initial_Expenses = [
+  { title: 'Toilet paper', amount: 94.12, date: new Date(2021, 2, 26) },
+  { title: 'Car insurance', amount: 294.67, date: new Date(2021, 4, 12) },
+  { title: 'Pizza', amount: 10, date: new Date(2021, 0, 1) }
+]
+
 const App = ()=> {
-  const expenses = [
-    { title: 'Toilet paper', amount: 94.12, date: new Date(2021, 2, 26) },
-    { title: 'Car insurance', amount: 294.67, date: new Date(2021, 4, 12) },
-    { title: 'Pizza', amount: 10, date: new Date(2021, 0, 1) }
-  ]
+  const [expenses, setExpenses] = useState(Initial_Expenses);
 
   const addExpenseHandler = expense => {
-    console.log('In app.js');
-    console.log(expense);
-    // expenses.push(expense);
+    setExpenses((prevExpenses => {
+      return [expense, ...prevExpenses]
+    }))
   };
   
   return (
